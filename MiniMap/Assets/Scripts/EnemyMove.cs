ba1a2500537d7ab9e7726584c1ba0ee7e4fd34d8
KIPKIPS <2432719656@qq.com>
2021-05-11 00:57:27 +0800

    Texture实现的小地图

---
+++ MiniMap/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+    // Start is called before the first frame update
+    private float speed = 4;
+    private float time = 2;
+    private float x = 0;
+    private float z = 0;
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        time += Time.deltaTime;
+        if (time >= 2) {
+            time = 0;
+            x = Random.Range(-1f, 1f);
+            z = Random.Range(-1f, 1f);
+        }
+        transform.Translate(new Vector3(x, 0, z) * speed * Time.deltaTime);
+    }
+}
--- MiniMap/Assets/Scripts/PlayerHandler.cs
+++ MiniMap/Assets/Scripts/PlayerHandler.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHandle : MonoBehaviour {
+public class PlayerHandler : MonoBehaviour {
     // Start is called before the first frame update
+    public float speed = 4f;
     void Start() {
 
     }
 
     // Update is called once per frame
     void Update() {
-
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(h, 0, v) * speed * Time.deltaTime);
     }
 }
